3c9e5483f16a5da6bb3eb78c25e5af2bd85be6a4
jatin0670 <jp0670786@gmail.com>
2025-05-15 20:07:54 +0530

    initial files

---
+++ ATM (Basic)/Bank.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class Bank {
+
+    public static void main(String[] args) {
+    
+        Scanner in = new Scanner(System.in);
+        
+       
+
+        int balance = 1000;
+        int x;
+        manu();
+
+        System.out.println("----------------------------------");
+        do{
+           
+        System.out.print("Enter Opt: ");
+           x = in.nextInt(); 
+           
+           switch (x) {
+            case 1:
+            System.out.print("Enter Amt: ");
+            int val = in.nextInt();
+            balance = add(balance, val);
+            System.out.println(balance); 
+            break;
+
+            case 2:
+            System.out.print("Enter Amt: ");
+            int val2 = in.nextInt();
+            balance = wit(balance, val2);
+            System.out.println(balance);
+            break;
+
+            case 3:
+            System.out.println("Your acc balance is: " + balance);
+            break;
+        
+            default:
+            System.out.println("not found");
+                break;
+            }
+            
+        }while(x!=0);
+
+        in.close();
+
+    }
+
+        
+
+    static void manu(){
+        System.out.println("1: for add balance");
+        System.out.println("2: minus balance");
+        System.out.println("3: balance check");
+    }
+
+
+    static int add(int bal, int amt){
+      return bal + amt;
+    
+    }
+
+    static int wit(int bal, int amt){
+        return bal - amt;
+    }
+
+
+
+
+}
